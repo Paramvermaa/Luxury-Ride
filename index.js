e9dd911592f047f1a7342f5f8547b8d9994c8183
Hi My name is Param Verma <112919138+Paramvermaa@users.noreply.github.com>
2023-05-21 21:06:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom/client"
-import App from "./src/components/App";
+import App from "../src/components/App";
 const container = document.getElementById('root');
 const root=ReactDom.createRoot(container);
 root.render(<App />);
